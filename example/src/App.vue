0d6b6d62a3991659894258b902ea917437291c92
ranckprogram <1474574022@qq.com>
2019-11-20 00:14:55 +0800

    重新部署项目结构

---
+++ example/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <Screenshot></Screenshot>
+  </div>
+</template>
+
+<script>
+import Screenshot from "../../src/Screenshot";
+export default {
+  name: "app",
+  components: {
+    Screenshot
+  },
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import Screenshot from './Screenshot';
+export default Screenshot;
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.component('Screenshot', Screenshot);
+}
